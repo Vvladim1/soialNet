2c34f08a20722003bbfbf5acc3420b55c3388d3f
vladimir <vladim.volovenko@gmail.com>
2020-04-13 19:11:39 +0300

    75 - React JS - redux-form введение (login)

--- src/components/redux/redux-store.js
+++ src/components/redux/redux-store.js
@@ -4,6 +4,7 @@ import profileReducer from './profile-reducer';
 import userReducer from "./users-reducer";
 import authReducer from "./auth-reducer";
 import thunkMiddleWare  from "redux-thunk";
+import { reducer as formReducer } from 'redux-form'
 
 
 let redusers = combineReducers({
@@ -11,7 +12,8 @@ let redusers = combineReducers({
     dialogsPage: dialogsReducer,
     profilePage: profileReducer,
     usersPage: userReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 });
 let store = createStore(redusers, applyMiddleware(thunkMiddleWare));
 
